27559fc75f7c8530ade931aa92e10a54e549c490
Rafael Ernesto <rflernesto@gmail.com>
2023-07-09 15:41:10 -0400

    feat: add socket server

---
+++ src/main/java/org/chat/ChatSocket.java
@@ -0,0 +1,51 @@
+package org.chat;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.websocket.*;
+import jakarta.websocket.server.PathParam;
+import jakarta.websocket.server.ServerEndpoint;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@ServerEndpoint("/chat/{username}")
+@ApplicationScoped
+public class ChatSocket {
+    Map<String, Session> sessions = new ConcurrentHashMap<>();
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("username") String username) {
+        sessions.put(username, session);
+    }
+
+    @OnClose
+    public void onClose(@PathParam("username") String username) {
+        sessions.remove(username);
+        broadcast("User " + username + " left the chat");
+    }
+
+    @OnError
+    public void onError(Session session, @PathParam("username") String username, Throwable throwable) {
+        sessions.remove(username);
+        broadcast("User " + username + " left on error");
+    }
+
+    @OnMessage
+    public void onMessage(String message, @PathParam("username") String username) {
+        if (message.equalsIgnoreCase("_ready_")) {
+            broadcast("User " + username + " joined the chat");
+        } else {
+            broadcast(username + ": " + message);
+        }
+    }
+
+    private void broadcast(String message) {
+        sessions.values().forEach(s -> {
+            s.getAsyncRemote().sendObject(message, result -> {
+                if (result.getException() != null) {
+                    System.out.println("Unable to send message: " + result.getException());
+                }
+            });
+        });
+    }
+}
